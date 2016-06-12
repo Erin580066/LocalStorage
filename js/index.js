241ef5e90c43c2a867039e0e3e559769adcc3dff
Erin580066 <597516068@qq.com>
2016-06-12 23:54:47 +0800

    num2

--- js/index.js
+++ js/index.js
@@ -7,12 +7,34 @@ var oS = '';
 oS = window.localStorage.getItem('list');
 for (var i = 0; i < lis.length; i++) {
 	lis[i].onclick = function(){
-		var value = this.innerHTML;
-		arr.push(value);
-		arr = fnQc(arr);
+		var value = this.innerHTML;//当前点击的内容
+		arr.push(value);//当前点击的内容放到数组里
+		arr = fnQc(arr);//数组去重
+		window.localStorage.setItem('list',arr);//去重好的值存到localstorage中
+		showList(arr);//渲染到页面
+	}
+};
+//选中删除
+list2.onclick = function(ev){
+	//如果点到了已选商品的li
+	if(ev.target.tagName == 'LI'){
+		//先拿到要删除的内容
+		var removeValue = ev.target.innerHTML;
+		//数组筛选
+		for(var i=0;i<arr.length;i++){
+			if(arr[i] == removeValue){
+				arr.splice(i,1);
+				i--;
+			}
+		}
+		//渲染页面
 		showList(arr);
+		//将新的数组给localStorage
+		window.localStorage.setItem('list',arr);
+		console.log(arr)
 	}
 }
+//渲染页面
 function showList(arr){
 	var str = '';
 	for (var i = 0; i < arr.length; i++) {
@@ -20,6 +42,7 @@ function showList(arr){
 	}
 	list2.innerHTML = str;
 }
+//数组去重
 function fnQc(arr){
 	var arr2 = [];
 	var json = {};
